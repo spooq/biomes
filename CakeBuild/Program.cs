b0245fa0944fcfb6f31ad4af1968f4c962c57e3d
Rob Bailey (actioninja) <actioninja@criticalaction.net>
2025-11-17 04:42:26 -0800

    Name I missed

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -26,7 +26,7 @@ public static class Program
 
 public class BuildContext : FrostingContext
 {
-    public const string ProjectName = "biomes_rebuilt";
+    public const string ProjectName = "biomes";
     public string BuildConfiguration { get; }
     public string Version { get; }
     public string Name { get; }
